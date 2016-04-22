08e7e3599ec32758874ef73facf4d876e6011058
alsxilva <ale10fut@hotmail.com>
2016-04-22 02:15:57 +0000

    Java

---
+++ Users.java
@@ -0,0 +1,11 @@
+import java.io.*
+
+public class Users{
+	 public static void main(String[] args) {
+
+	 	Buffered Reader din = new Buffered (new InputStreamReader(System.in));
+	 	System.out.println("\n\t\tBienvenido al programa que busca nicknames de Twitter. \n\t\tPor favor, introduce la ruta del archivo a utilizar:");
+	 	string origen = din.readLine();
+	 	new 
+	 }
+}
