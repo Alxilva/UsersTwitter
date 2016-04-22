d93ded6de2cc712235cd4f1cd3e6c27ff8a4ed59
alsxilva <ale10fut@hotmail.com>
2016-04-22 02:29:18 +0000

    Pide y muestra documento de texto

--- Users.java
+++ Users.java
@@ -3,9 +3,16 @@ import java.io.*;
 public class Users{
 	 public static void main(String[] args)throws IOException {
 
-	 	BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+	 	BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
 	 	System.out.println("\n\t\tBienvenido al programa que busca nicknames de Twitter. \n\t\tPor favor, introduce la ruta del archivo a utilizar:");
-	 	String origen = din.readLine();
-	 	PrintWriter pout = new PrintWriter(new FileWriter(origen));
-	 }
+	 	String origen = leer.readLine();
+	 	BufferedReader  archivo  =  new BufferedReader(new FileReader(origen));
+	 	System.out.println("Contenido del archivo");
+	String linea = archivo.readLine();
+	while(linea != null){
+		System.out.println(linea);
+		linea = archivo.readLine();
+	}
+	archivo.close();
+}
 }
