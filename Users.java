d54902cfd535173464dc604e94230f5fe04e0a87
alsxilva <ale10fut@hotmail.com>
2016-04-22 03:37:49 +0000

    Intentando separar los usuarios mediante el arroba

--- Users.java
+++ Users.java
@@ -4,6 +4,7 @@ import java.lang.*;
 public class Users{
 	 public static void main(String[] args)throws IOException {
 	 	char arroba = '@';
+	 	char espacio = ' ';
 
 	 	BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
 	 	System.out.println("\n\t\tBienvenido al programa que busca nicknames de Twitter. \n\t\tPor favor, introduce la ruta del archivo a utilizar:");
@@ -15,7 +16,10 @@ public class Users{
 		
 		while(linea != null){
 			System.out.println(linea);
-			System.out.println("La posicion es: " + linea.indexOf(arroba));
+			System.out.println("En la posicion " + linea.indexOf(arroba) + " hay un nickname de usuario.");
+			int identificador = linea.indexOf(arroba);
+			int idenafteruser = substring(identificador).indexOf(espacio);
+			System.out.println("1. " + linea.substring(identificador,idenafteruser));
 			linea = archivo.readLine();
 			
 		}
